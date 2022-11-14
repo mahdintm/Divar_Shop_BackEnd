c035995277037e31b94b2caad01e594c9784ec83
Mahdi Nemati <nematimahdi88@gmail.com>
2022-11-15 00:56:16 +0330

    .

--- router/api.js
+++ router/api.js
@@ -30,3 +30,31 @@ apiRouter.get("/category", (req, res) => {
 apiRouter.get("/user", async (req, res) => {
   res.send(await sql(`select * from Users where id="${req.query.id}"`));
 });
+
+apiRouter.get("/RegisterProduct", async (req, res) => {
+  Product_data.forEach(async (element) => {
+    if (element.id == req.query.Product_id) {
+      if (element.registrations.length != 4 && !element.registrations.find((a) => a == 1)) {
+        await element.registrations.push(req.query.User_id);
+        await sql(`update Products set registrations='${JSON.stringify(element.registrations)}' where id="${element.id}"`);
+        res.send({ res: true });
+      } else {
+        res.send({ res: false });
+      }
+    }
+  });
+});
+apiRouter.get("/removeRegisterProduct", async (req, res) => {
+  Product_data.forEach(async (element) => {
+    if (element.id == req.query.Product_id) {
+      if (element.registrations.find((a) => a == req.query.User_id)) {
+        var index = await element.registrations.indexOf(req.query.User_id);
+        if (index != -1) element.registrations.splice(index, 1);
+        await sql(`update Products set registrations='${JSON.stringify(element.registrations)}' where id="${element.id}"`);
+        res.send({ res: true });
+      } else {
+        res.send({ res: false });
+      }
+    }
+  });
+});
